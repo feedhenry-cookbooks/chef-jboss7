76e79c1e9c2cc6ff3afe24171342a5c36cba6adc
Matthias Wessendorf <matzew@apache.org>
2016-03-03 11:26:19 +0100

    Bump to next version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'andy.k.dufour@gmail.com'
 license 'All rights reserved'
 description 'Installs/Configures jboss7'
 long_description 'Installs/Configures jboss7'
-version '0.11.7'
+version '0.11.8'
 
 depends 'apt'
 depends 'java'
